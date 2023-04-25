2fe9c166f2e0e3355e3c4edc089bd0af3305616f
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-04-25 14:40:24 +0000

    python tlg init

---
+++ lab_input/dayofweek.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def main():
+
+    name = input("What.... is your name??")
+    dayofweek= input("WHAT???.... is the day of the week?")
+
+    print(f"Hello, { name }!  Happy { dayofweek }!")
+
+
+main()
+
+
