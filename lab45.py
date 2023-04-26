94a29b93001e9a6c1c2b1e8de52988b364821867
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-04-26 19:25:17 +0000

    working on if, elif, else logic

---
+++ lab45.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Conditionals - strings test true"""
+
+ipchk = input("Please apply an IP address:\n")
+
+# a string tests as True
+if ipchk:
+   print("Looks like the IP address was set: " + ipchk)
+else:
+    print("You did not provide input")
+    
