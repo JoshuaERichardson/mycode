5ccb4dd2e33a2cfa7c2a98b85d1efa6772c83bab
joshuaerichardson <joshuarichardson1478@gmail.com>
2023-05-04 17:44:16 +0000

    serving cookies

---
+++ flaskapi/milkncookies.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from flask import (
+        Flask,
+        make_response,
+        request,
+        render_template,
+        redirect,
+        url_for
+        )
+app = Flask(__name__)
+
+@app.route("/login")
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+@app.route("/setcookie", methods = ["POST", "GET"])
+def setcookie():
+    if request.method == "POST":
+        if request.form.get("nm"):
+            user = request.form.get("nm")
+        else:
+            user = 'defaultuser'
+
+        resp = make_response(render_template("readcookie.html"))
+        resp.set_cookie("userID", user)
+
+        return resp
+    if request.method == "GET":
+        return redirect(url_for("index"))
+# check users cookie for their name
+@app.route("/getcookie")
+def getcookie():
+    name = request.cookies.get("userID")
+    return f'<h1>Welcome to {name}</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
+
+
+
