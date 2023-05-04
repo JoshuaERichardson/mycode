6548b1fb29a41d53fcf11e0db6079457a021d305
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-05-04 11:09:55 -0600

    pushing non working code for project3

---
+++ Project3/alta3research-requests02.py
@@ -0,0 +1,11 @@
+'''
+
+Goal:
+Send a GET request to your flask API; it should target the endpoint that returns JSON
+Take the returned JSON and 'normalize' it into a format that is easy for users to understand
+
+author: joshuaerichardson
+date: 2023-05-04
+
+'''
+
