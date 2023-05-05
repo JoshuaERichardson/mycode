ebc1455f8b3e2f49b731ca543b69cd4da08b0984
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-05-04 20:11:36 -0600

    login functionality

--- Project3/alta3research-requests02.py
+++ Project3/alta3research-requests02.py
@@ -8,4 +8,15 @@ author: joshuaerichardson
 date: 2023-05-04
 
 '''
+from flask import Flask, request, jsonify
+from pprint import pprint
+
+URL = "http://127.0.0.1:2224"
+
+def main():
+    r = request.get(URL + "/api/v1/users")
+    pprint(r.json())
+
+if __name__ == "__main__":
+    main()
 
